8573329a126386b2ad66036aaf6edea1ebd23425
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-05-16 11:01:37 +0200

    Added basic navbar for main page

---
+++ bookit-front/src/components/Main/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul class="nav nav-tabs">
+    <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+    </li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Settings</a>
+        <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Separated link</a>
+        </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+    </li>
+</ul>
+    
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  computed:{
+  },
+  mounted() {
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+  }
+</script>
+
+<style scoped>
+
+</style>
