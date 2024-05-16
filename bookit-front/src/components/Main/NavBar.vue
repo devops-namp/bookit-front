b21dd3d0f0253d79fd61822d9a0814cfb1511ac3
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-05-16 16:19:25 +0200

    feature: added user recognition in main page nav-bar

--- bookit-front/src/components/Main/NavBar.vue
+++ bookit-front/src/components/Main/NavBar.vue
@@ -6,25 +6,24 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item dropdown">
+                <li v-if="user" class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Aleksa Simic
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#"><i class="bi bi-person-fill"/>&nbsp;Manage account</a>
-                    <a class="dropdown-item" href="#"><font-awesome-icon icon="fa-solid fa-suitcase"/>&nbsp;Booking and trips</a>
-                    <a class="dropdown-item" href="#"><i class="bi bi-person-hearts"/>&nbsp;Reviews</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#"><i class="bi bi-box-arrow-left"/>&nbsp;Sign out</a>
+                        <a class="dropdown-item" href="#"><i class="bi bi-person-fill"/>&nbsp;Manage account</a>
+                        <a class="dropdown-item" href="#"><font-awesome-icon icon="fa-solid fa-suitcase"/>&nbsp;Booking and trips</a>
+                        <a class="dropdown-item" href="#"><i class="bi bi-person-hearts"/>&nbsp;Reviews</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#"><i class="bi bi-box-arrow-left"/>&nbsp;Sign out</a>
                     </div>
                 </li>
             </ul>
             <a class="text-light text-decoration-none p-2" href="#">List your property</a>
-            <a class="text-light text-decoration-none bg-info p-2" href="#">Register</a>
-            <a class="text-light text-decoration-none bg-info p-2" href="#">Sign in</a>
+            <a v-if="!user" class="text-light text-decoration-none bg-info p-2" href="#">Register</a>
+            <a v-if="!user" class="text-light text-decoration-none bg-info p-2" href="#">Sign in</a>
         </div>
     </nav>
-    
 </template>
 
 <script>
@@ -36,6 +35,7 @@ export default {
   },
   data() {
     return {
+        user: 'nesto'
     };
   },
   methods: {
