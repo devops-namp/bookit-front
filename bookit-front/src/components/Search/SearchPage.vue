0a9459e6886835fe84ba7a4db9a1bf9e89e780e9
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-05-17 12:37:55 +0200

    feat: added filters on the search page

--- bookit-front/src/components/Search/SearchPage.vue
+++ bookit-front/src/components/Search/SearchPage.vue
@@ -1,25 +1,80 @@
 <template>
   <div class="bg-dark entire-div">
     <div class="container">
-      <nav-bar/>
-      <search-bar/>
+      <nav-bar />
+      <search-bar />
+      <div class="row mt-4">
+        <div class="col-md-3">
+          <div class="filter-section">
+            <h5>Filters</h5>
+            <div class="form-group">
+              <label for="priceRange">Price Range</label>
+              <vue3-slider v-model="priceRange" :min="0" :max="1000" :tooltip="'always'"/>
+              <div class="d-flex justify-content-between">
+                <span>{{ priceRange[0] }}$</span>
+                <span>{{ priceRange[1] }}$</span>
+              </div>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="wifi" v-model="filters.wifi" />
+              <label class="form-check-label" for="wifi">Wi-Fi</label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="kitchen" v-model="filters.kitchen" />
+              <label class="form-check-label" for="kitchen">Kitchen</label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="airCondition" v-model="filters.airCondition" />
+              <label class="form-check-label" for="airCondition">Air conditioning</label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="freeParking" v-model="filters.freeParking" />
+              <label class="form-check-label" for="freeParking">Free parking</label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="wc" v-model="filters.wc" />
+              <label class="form-check-label" for="wc">Inner toilet</label>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-
 <script>
 import NavBar from '../util/NavBar.vue'
 import SearchBar from '../util/SearchBar.vue'
+import Vue3Slider from 'vue3-slider'
 
 export default {
   name: 'SearchPage',
   components: {
     NavBar,
     SearchBar,
+    Vue3Slider
   },
   data() {
     return {
+      priceRange: [100, 500],
+      filters: {
+        wifi: false,
+        kitchen: false,
+        airCondition: false,
+        freeParking: false,
+        wc: false
+      }
+    }
+  },
+  watch: {
+    priceRange(newVal) {
+      console.log('Price range changed:', newVal)
+    },
+    filters: {
+      handler(newVal) {
+        console.log('Filters changed:', newVal)
+      },
+      deep: true
     }
   }
 }
@@ -29,9 +84,9 @@ export default {
 .entire-div {
   min-height: 100vh;
 }
-.img-responsive {
-  width: 100%;
-  max-height: 70vh; 
-  margin: auto;
+.filter-section {
+  background: #fff;
+  padding: 15px;
+  border-radius: 5px;
 }
 </style>
