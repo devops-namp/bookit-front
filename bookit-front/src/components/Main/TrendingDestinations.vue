6735c6e6b2f79111ecc473d0982ada9e78f5c960
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-05-17 11:32:37 +0200

    refactor: main page split up into components SearchBar and TrendingDestinations

---
+++ bookit-front/src/components/Main/TrendingDestinations.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="mt-4">
+        <h2 class="text-light mb-3">Trending destinations</h2>
+        <div class="row">
+            <div class="col">
+            <a href="#" target="_blank">
+                <img src="../../assets/Nis.jpg" alt="Destination 1" class="img-thumbnail">
+            </a>
+            </div>
+            <div class="col">
+            <a href="#" target="_blank">
+                <img src="../../assets/Kopaonik.jpg" alt="Destination 2" class="img-thumbnail">
+            </a>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col">
+            <a href="#" target="_blank">
+                <img src="../../assets/Belgrade.jpg" alt="Destination 3" class="img-thumbnail">
+            </a>
+            </div>
+            <div class="col">
+            <a href="#" target="_blank">
+                <img src="../../assets/NoviSad.jpeg" alt="Destination 4" class="img-thumbnail">
+            </a>
+            </div>
+            <div class="col">
+            <a href="#" target="_blank">
+                <img src="../../assets/Zlatibor.jpg" alt="Destination 5" class="img-thumbnail">
+            </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TrendingDestinations',
+  computed:{
+  },
+  mounted() {
+  },
+  data() {
+    return {
+        user: ''
+    };
+  },
+  methods: {
+  },
+  }
+</script>
+
+<style scoped>
+
+</style>
