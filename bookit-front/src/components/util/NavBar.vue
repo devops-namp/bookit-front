f04327cfab671fbd3c96bec4b14ce060c82608c5
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-05-20 14:20:03 +0200

    feat: added users reservations page

--- bookit-front/src/components/util/NavBar.vue
+++ bookit-front/src/components/util/NavBar.vue
@@ -13,7 +13,7 @@
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="/profile"><i class="bi bi-person-fill"/>&nbsp;Manage account</a>
                         <a class="dropdown-item" href="/tripHistory"><font-awesome-icon icon="fa-solid fa-suitcase"/>&nbsp;Booking and trips</a>
-                        <a class="dropdown-item" href="#"><i class="bi bi-person-hearts"/>&nbsp;Reviews</a>
+                        <a class="dropdown-item" href="#"><i class="bi bi-person-hearts"/>&nbsp;Reservations</a>
                         <div class="dropdown-divider"></div> <a class="dropdown-item" href="#"><i class="bi bi-box-arrow-left"/>&nbsp;Sign out</a>
                     </div>
                 </li>
