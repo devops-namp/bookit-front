d55072657837dfda68d9b305232971a34d2c7415
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-06-14 15:49:58 +0200

    fix: persisted state added for totalPrice for reservation

--- bookit-front/src/store/store.js
+++ bookit-front/src/store/store.js
@@ -1,14 +1,19 @@
 import { createStore } from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 export default createStore({
   state: {
     trip: {},
+    totalPrice: null,
     searchFromDate: null,
     searchToDate: null,
   },
   mutations: {
     SET_TRIP(state, trip) {
       state.trip = trip;
+    },
+    SET_TOTALPRICE(state, totalPrice) {
+      state.totalPrice = totalPrice;
     },
     UPDATE_TRIP_PROPERTY(state, { property, value }) {
       state.trip[property] = value;
@@ -28,6 +33,9 @@ export default createStore({
     setTrip({ commit }, trip) {
       commit('SET_TRIP', trip);
     },
+    setTotalPrice({ commit }, totalPrice) {
+      commit('SET_TOTALPRICE', totalPrice);
+    },
     updateTripProperty({ commit }, { property, value }) {
       commit('UPDATE_TRIP_PROPERTY', { property, value });
     }
@@ -35,6 +43,15 @@ export default createStore({
   getters: {
     getTrip(state) {
       return state.trip;
+    },
+    getTotalPrice(state) {
+      return state.totalPrice;
     }
-  }
+  },
+  plugins: [createPersistedState({
+    storage: window.sessionStorage,
+    reducer: (state) => ({
+      totalPrice:state.totalPrice
+    })
+  })]
 });
