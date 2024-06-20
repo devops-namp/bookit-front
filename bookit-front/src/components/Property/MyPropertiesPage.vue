0e9cfc979cdf442ba5756010eeb0cf705910a91e
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-06-20 23:22:00 +0200

    fix: owners accommodations are now showing

--- bookit-front/src/components/Property/MyPropertiesPage.vue
+++ bookit-front/src/components/Property/MyPropertiesPage.vue
@@ -6,7 +6,7 @@
         <h2 class="text-white mb-4">Manage properties</h2>
         <div class="property-card" v-for="property in properties" :key="property.id">
           <div class="d-flex justify-content-center p-5 col-3">
-            <img :src="property.images[0]" alt="Property Image" class="property-image">
+            <img :src="'data:image/jpeg;base64,' + property.images[0].base64Image"  alt="Property Image" class="property-image">
           </div>
           <div class="property-details col-3">
             <h6 class="h6-name">{{ property.name }}</h6>
@@ -14,14 +14,15 @@
             <p>Price type: <b class="price-p">{{ formatPriceType(property.priceType) }}</b></p>
           </div>
           <div class="property-details col">
-            <div class="align-content-center text-center">
+            <!-- <div class="align-content-center text-center">
               <div v-for="adjustment in property.priceAdjustments" :key="adjustment.price">
                 <p> 
                   <b class="color-it-blue">from:&nbsp;</b>{{ formatDate(adjustment.dates[0]) }}&nbsp;&nbsp;&nbsp;
                   <b class="color-it-blue">to:&nbsp;</b>{{ formatDate(adjustment.dates[1]) }}&nbsp;&nbsp;&nbsp;
-                  <b class="color-it-blue">for:&nbsp;</b><b class="color-it-green">{{ adjustment.price }}€</b></p>
+                  <b class="color-it-blue">for:&nbsp;</b><b class="color-it-green">{{ adjustment.price }}€</b>
+                </p>
               </div>
-            </div>
+            </div> -->
             <button class="btn btn-info" @click="updateProperty(property.id)">Update property</button>
           </div>
         </div>
@@ -33,6 +34,7 @@
 <script>
 import NavBar from "../util/NavBar.vue";
 import moment from "moment";
+import AccommodationService from '@/service/AccommodationService';
 
 export default {
   name: "MyPropertiesPage",
@@ -42,75 +44,86 @@ export default {
   data() {
     return {
       properties: [
-        {
-          id: 1,
-          name: "Central Konaci Apartments on APrtments on Apartments",
-          location: "Kopaonik",
-          filters: [],
-          minGuests: 1,
-          maxGuests: 1,
-          priceType: "price-per-unit",
-          images: [
-          "https://cf.bstatic.com/xdata/images/hotel/max1024x768/551076950.jpg?k=0cc401ec6cfc9c27e602d358c5a36afcd524c9bbafd93a1152edbad6208c564d&o=&hp=1",
-          "https://cf.bstatic.com/xdata/images/hotel/max1024x768/551077088.jpg?k=9de972a17f808f2f55930ca0538846e2383f3a2041934decd50fa69691453a07&o=&hp=1",
-          "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520536.jpg?k=4c3d970eed45c89579f39ca1e109dd01a31b12dbbd34f1fa417934fd919496dc&o=&hp=1",
-          "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520898.jpg?k=bfe2fe818abb30c41c7a2ec70bdfe560d251484e2e542abab0ea0a8931f170c2&o=&hp=1",
-          "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520539.jpg?k=9d713cd1709ee6a8aa44a94558557133f348e230e4d3f8235a210c989e301908&o=&hp=1",
-          "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520539.jpg?k=9d713cd1709ee6a8aa44a94558557133f348e230e4d3f8235a210c989e301908&o=&hp=1",
-          "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520909.jpg?k=f061149dd59c7a7a4fe9c51440f24eff26d009668f0704d9b7348482f3aef981&o=&hp=1",
-        ],
-          priceAdjustments: [
-            {
-              dates: [
-                new Date("2024-05-21T09:20:00"),
-                new Date("2024-05-31T09:20:00")
-              ],
-              price: 1555
-            }
-          ],
-        },
-        {
-        id: 2,
-        name: "Central Konaci Apartments",
-        location: "Kopaonik",
-        filters: [],
-        minGuests: 1,
-        maxGuests: 1,
-        priceType: "price-per-unit",
-        images: [
-          "https://cf.bstatic.com/xdata/images/hotel/max1024x768/551077088.jpg?k=9de972a17f808f2f55930ca0538846e2383f3a2041934decd50fa69691453a07&o=&hp=1",
-          "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520536.jpg?k=4c3d970eed45c89579f39ca1e109dd01a31b12dbbd34f1fa417934fd919496dc&o=&hp=1",
-          "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520898.jpg?k=bfe2fe818abb30c41c7a2ec70bdfe560d251484e2e542abab0ea0a8931f170c2&o=&hp=1",
-          "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520539.jpg?k=9d713cd1709ee6a8aa44a94558557133f348e230e4d3f8235a210c989e301908&o=&hp=1",
-          "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520539.jpg?k=9d713cd1709ee6a8aa44a94558557133f348e230e4d3f8235a210c989e301908&o=&hp=1",
-          "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520909.jpg?k=f061149dd59c7a7a4fe9c51440f24eff26d009668f0704d9b7348482f3aef981&o=&hp=1",
-        ],
-        priceAdjustments: [
-            {
-              dates: [
-                new Date("2024-06-01T09:20:00"),
-                new Date("2024-06-10T09:20:00")
-              ],
-              price: 1750
-            },
-            {
-              dates: [
-                new Date("2024-06-01T09:20:00"),
-                new Date("2024-06-10T09:20:00")
-              ],
-              price: 1750
-            },
-            {
-              dates: [
-                new Date("2024-06-01T09:20:00"),
-                new Date("2024-06-10T09:20:00")
-              ],
-              price: 1750
-            }
-          ],
-        }
+        // {
+        //   id: 1,
+        //   name: "Central Konaci Apartments on APrtments on Apartments",
+        //   location: "Kopaonik",
+        //   filters: [],
+        //   minGuests: 1,
+        //   maxGuests: 1,
+        //   priceType: "price-per-unit",
+        //   images: [
+        //   "https://cf.bstatic.com/xdata/images/hotel/max1024x768/551076950.jpg?k=0cc401ec6cfc9c27e602d358c5a36afcd524c9bbafd93a1152edbad6208c564d&o=&hp=1",
+        //   "https://cf.bstatic.com/xdata/images/hotel/max1024x768/551077088.jpg?k=9de972a17f808f2f55930ca0538846e2383f3a2041934decd50fa69691453a07&o=&hp=1",
+        //   "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520536.jpg?k=4c3d970eed45c89579f39ca1e109dd01a31b12dbbd34f1fa417934fd919496dc&o=&hp=1",
+        //   "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520898.jpg?k=bfe2fe818abb30c41c7a2ec70bdfe560d251484e2e542abab0ea0a8931f170c2&o=&hp=1",
+        //   "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520539.jpg?k=9d713cd1709ee6a8aa44a94558557133f348e230e4d3f8235a210c989e301908&o=&hp=1",
+        //   "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520539.jpg?k=9d713cd1709ee6a8aa44a94558557133f348e230e4d3f8235a210c989e301908&o=&hp=1",
+        //   "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520909.jpg?k=f061149dd59c7a7a4fe9c51440f24eff26d009668f0704d9b7348482f3aef981&o=&hp=1",
+        // ],
+        //   priceAdjustments: [
+        //     {
+        //       dates: [
+        //         new Date("2024-05-21T09:20:00"),
+        //         new Date("2024-05-31T09:20:00")
+        //       ],
+        //       price: 1555
+        //     }
+        //   ],
+        // },
+        // {
+        // id: 2,
+        // name: "Central Konaci Apartments",
+        // location: "Kopaonik",
+        // filters: [],
+        // minGuests: 1,
+        // maxGuests: 1,
+        // priceType: "price-per-unit",
+        // images: [
+        //   "https://cf.bstatic.com/xdata/images/hotel/max1024x768/551077088.jpg?k=9de972a17f808f2f55930ca0538846e2383f3a2041934decd50fa69691453a07&o=&hp=1",
+        //   "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520536.jpg?k=4c3d970eed45c89579f39ca1e109dd01a31b12dbbd34f1fa417934fd919496dc&o=&hp=1",
+        //   "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520898.jpg?k=bfe2fe818abb30c41c7a2ec70bdfe560d251484e2e542abab0ea0a8931f170c2&o=&hp=1",
+        //   "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520539.jpg?k=9d713cd1709ee6a8aa44a94558557133f348e230e4d3f8235a210c989e301908&o=&hp=1",
+        //   "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520539.jpg?k=9d713cd1709ee6a8aa44a94558557133f348e230e4d3f8235a210c989e301908&o=&hp=1",
+        //   "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554520909.jpg?k=f061149dd59c7a7a4fe9c51440f24eff26d009668f0704d9b7348482f3aef981&o=&hp=1",
+        // ],
+        // priceAdjustments: [
+        //     {
+        //       dates: [
+        //         new Date("2024-06-01T09:20:00"),
+        //         new Date("2024-06-10T09:20:00")
+        //       ],
+        //       price: 1750
+        //     },
+        //     {
+        //       dates: [
+        //         new Date("2024-06-01T09:20:00"),
+        //         new Date("2024-06-10T09:20:00")
+        //       ],
+        //       price: 1750
+        //     },
+        //     {
+        //       dates: [
+        //         new Date("2024-06-01T09:20:00"),
+        //         new Date("2024-06-10T09:20:00")
+        //       ],
+        //       price: 1750
+        //     }
+        //   ],
+        // }
       ],
     };
+  },
+  mounted() {
+    const ownerUsername = localStorage.getItem("username");
+    AccommodationService.getOwnersAccommodations(ownerUsername)
+      .then(response => {
+        console.log(response.data);
+        this.properties = response.data;
+      })
+      .catch(error => {
+        console.error("Error fetching accommodations:", error);
+      });
   },
   methods: {
     formatPriceType(priceType){
